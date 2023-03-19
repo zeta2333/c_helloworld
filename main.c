0a56be5263363cc09097624a738b49f6feca3bfd
Pycro <2023747015@qq.com>
2023-03-19 21:08:37 +0800

    HelloWorld update

--- main.c
+++ main.c
@@ -1,13 +1,10 @@
-#include <stdio.h>
-
-int main() {
-    float a = 0.302;
-    float b = 128.101;
-    double c = 123;
-    float d = 112.64E3;
-    double e = 0.7623e-2;
-    float f = 1.23002398;
-    printf("a=%e \nb=%f \nc=%lf \nd=%lE \ne=%lf \nf=%f\n", a, b, c, d, e, f);
+#include<stdio.h>
+int main(){
+    int sum = 103;  //总数
+    int count = 7;  //数目
+    double average;  //平均数
+    char* str = (char *)sum;
+    average = (double) sum / count;
+    printf("Average is %lf!\n", average);
     return 0;
 }
-
