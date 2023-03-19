d64a4d5b551d6a71329f02fcc0b1da17335a62bb
Pycro <2023747015@qq.com>
2023-03-19 15:33:51 +0800

    HelloWorld update

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    short a = 0b1010110;  //二进制数字
-    int b = 02713;  //八进制数字
-    long c = 0X1DAB83;  //十六进制数字
-
-    printf("a=%#ho, b=%#o, c=%#lo\n", a, b, c);  //以八进制形似输出
-    printf("a=%hd, b=%d, c=%ld\n", a, b, c);  //以十进制形式输出
-    printf("a=%#hx, b=%#x, c=%#lx\n", a, b, c);  //以十六进制形式输出（字母小写）
-    printf("a=%#hX, b=%#X, c=%#lX\n", a, b, c);  //以十六进制形式输出（字母大写）
+    //负数
+    short a1 = -10;
+    short a2 = -0x2dc9;  //十六进制
+//正数
+    int b1 = +10;
+    int b2 = +0174;  //八进制
+    int b3 = 22910;
+//负数和正数相加
+    long c = (-9) + (+12);
     return 0;
 }
 
