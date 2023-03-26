b3d744c65b2b65dd2b1e553c2d00a2f9506b4720
Pycro <2023747015@qq.com>
2023-03-26 17:58:05 +0800

    3.26 update

--- main.c
+++ main.c
@@ -1,24 +1,12 @@
 #include <stdio.h>
-
-//返回两个数中较大的一个
-int max(int a, int b) {
-    return a > b ? a : b;
-}
-
-int sum(int a, int b) {
-    return a + b;
-}
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    int x, y, maxval;
-    //定义函数指针
-    int (*pmax)(int, int) = max;  //也可以写作int (*pmax)(int a, int b)
-    int (*s)(int, int) = sum;
-    int res = (*s)(2, 3);
-    printf("%d\n", res);
-    printf("Input two numbers:");
-//    scanf("%d %d", &x, &y);
-    maxval = (*pmax)(2, 3);
-    printf("Max value: %d\n", maxval);
+    FILE *fp;
+    if ((fp = fopen("./demo.txt", "r")) == NULL) {
+        printf("打开文件失败\n");
+        exit(0);
+    }
     return 0;
 }
